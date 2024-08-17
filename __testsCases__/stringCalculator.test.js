b801b9a2f22b31a91f9dc2e4217f028d512743ef
shreyv8991 <0shreyverma0@gmail.com>
2024-08-17 20:05:02 +0530

    Handle single number input

--- __testsCases__/stringCalculator.test.js
+++ __testsCases__/stringCalculator.test.js
@@ -1,5 +1,11 @@
 const add = require('../stringCalculator');
 
+//Handle empty string
 test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
+});
+
+//Handle single number input
+test('should return the number itself if only one number is provided', () => {
+    expect(add("1")).toBe(1);
 });
